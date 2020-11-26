09532cb4fa7c96aff0f4908a4d0af3297e52e80d
rlawoehd187 <rlawoehd187@naver.com>
2020-11-26 23:04:49 +0900

    ef71: Update time services from TA-1003

--- Android.mk
+++ Android.mk
@@ -68,7 +68,7 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE := TimeService
 LOCAL_MODULE_OWNER := pantech
-LOCAL_SRC_FILES := proprietary/app/TimeService/TimeService.apk
+LOCAL_SRC_FILES := proprietary/vendor/app/TimeService/TimeService.apk
 LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
